05749577956c6598e503bd1fc89600ed3d109912
Kiep13 <kiep13@bk.ru>
2021-06-25 10:09:37 +0300

    feat: add material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,32 @@ import { SlotComponent } from './components/slot/slot.component';
 import { BoardComponent } from './components/board/board.component';
 import {NgxsModule} from "@ngxs/store";
 import {PlayStore} from "./store/store";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './components/header/header.component';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent,
     SlotComponent,
-    BoardComponent
+    BoardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
 
-    NgxsModule.forRoot([PlayStore])
+    NgxsModule.forRoot([PlayStore]),
+
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
